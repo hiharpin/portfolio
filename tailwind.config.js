7157c5c6930bc6e163d8b833731fd0bb6e14d057
Achmad Harpin Asrori <23521086@std.stei.ac.id>
2022-04-21 04:30:27 +0700

    Upload to github

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        darkblue: '#0284c7',
+        black: '#171717',
+        neutral: '#737373',
+        softblue: '#f0f9ff',
+        gray: '#9ca3af'
+      },
+      screen: {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
